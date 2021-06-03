948cfe1fff61444cb8986a1ac33b32720c927f03
Shoshone Arnold <eastcoastshony@gmail.com>
2021-06-03 19:44:39 -0400

    updated pages

--- pages/components/InfoStart.js
+++ pages/components/InfoStart.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import test from '../../public/flatline.svg'
 
 const InfoStart = () => {
     return (
         <div className="info-start-cont">
             <div className="info-start">
-                
+                <div></div>
+                <div></div>
+                <img className="imagestart" src={test}></img>
+                <div></div>
+                <h2>Create an invite-only place where you belong</h2>
             </div>
         </div>
     )
